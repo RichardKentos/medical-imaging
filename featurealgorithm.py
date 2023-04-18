c5ee63dd17020881f8e89128d476bba508da4ad3
esma <esma@itu.dk>
2023-04-18 18:19:39 +0200

    added a not working symmetry check

--- featurealgorithm.py
+++ featurealgorithm.py
@@ -6,6 +6,29 @@ from pathlib import Path
 from skimage.transform import resize
 from prepareImage import *
 
+def is_symmetric(matrix):
+    """
+    Determines if a given binary matrix is symmetric.
+    Args:
+        matrix (list of lists): A binary matrix.
+    Returns:
+        bool: True if the matrix is symmetric, False otherwise.
+    """
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    # Check if the number of rows equals the number of columns
+    if rows != cols:
+        return False
+
+    # Check if the matrix is symmetric
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] != matrix[j][i]:
+                return False
+
+    return True
+
 # Prepare an image
 filename = 'PAT_72_110_647.png'
 im1 = prepareImage(filename)
@@ -26,3 +49,8 @@ pixels2 = pixels2.astype(np.int8)
 print(pixels2)
 
 rot_im = transform.rotate(im2, 40)
+
+#is_symmetric(pixels2)
+
+
+
