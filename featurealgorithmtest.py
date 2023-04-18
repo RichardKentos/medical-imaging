f6eca8f128a7e2e2b60218855ff5b86fe43b2ed8
Marija Trpenovska <mtrpenovska@gmail.com>
2023-04-18 18:05:30 +0200

    test code

--- featurealgorithmtest.py
+++ featurealgorithmtest.py
@@ -3,10 +3,12 @@ from PIL import Image
 def is_asymmetric(image):
     image = image.convert('L')
     flipped_image = image.transpose(method=Image.FLIP_LEFT_RIGHT)
-    return image.tobytes() != flipped_image.tobytes()
+    image_data = list(image.getdata())
+    flipped_data = list(flipped_image.getdata())
+    return image_data != flipped_data
 
 # Replace 'image.png' with the path to your PNG image file
-image = Image.open('data/images/guide_images/PAT_8_15_820.png')
+image = Image.open('data/images/guide_images/PAT_89_135_11.png')
 if is_asymmetric(image):
     print('The image is asymmetric')
 else:
