954de38a26f5e502ecd51a524078aedd8ea918ac
Richard <richardkentos@gmail.com>
2023-04-18 17:38:16 +0200

    prepareImage function fixed

--- prepareImage.py
+++ prepareImage.py
@@ -3,10 +3,9 @@ from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 
 
-def prepareImage(path):
-    filename = Path(f'data/images/imgs_part_1/{filename}')  # PAT_8_15_820.png
-    file_im = Path(f'data/images/imgs_part_1/{filename}')  # PAT_8_15_820.png
-    im = plt.imread(file_im)
+def prepareImage(imageName):
+    filename = Path(f'data/images/imgs_part_1/{imageName}')  # PAT_8_15_820.png
+    im = plt.imread(filename)
     im2 = im[0:1500, :, :3]  # Select only the first three channels (RGB)
     im2 = rgb2gray(im2)*256
     return im2
