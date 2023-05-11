d3986a63bf9d4769904c854785809c313bebec67
EDBM <seve0705@gmail.com>
2023-05-11 18:19:21 +0200

    made the main function write to featureFile.csv file

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ from colourfeaturetest import *
 from symmetrytest import *
 from diameter import *
 from imagesegmentation import *
+import csv
+import os
 
 def main():
     # loop through each image in the folder and apply the appropiate methods and write to the csv file
     # for each image in folder
-        # segmented_image = segment the image method
-        # symmetry = symmetrymethod(segmented_image)
-        # color_variance = colormethod(segmented_image)
-        # diameter = diametermethod(segmented_image)
-
-        # write the results to .csv file, maybe make a method for that?
-    segmented_image = segmentImage("data/images/guide_images/PAT_72_110_647.png")
+    image_id = os.path.basename(os.path.normpath('data/images/guide_images/PAT_72_110_647.png'))
     symmetry = getSymmetry("data/images/guide_images/PAT_72_110_647.png")
     Rvar, gvar, bvar = exact_color("data/images/guide_images/PAT_72_110_647.png", 900, 12, 2.5)
     diameter = getDiameter("data/images/guide_images/PAT_72_110_647.png")
-    print("Symmetry classifier of lesion: " + str(symmetry))
-    print("RGB Variance: R: " + str(Rvar) + " G: "+ str(gvar) + " B: "+str(bvar))
-    print("Diameter of lesion: " + str(diameter))
+
+    headers = ["image_id","asymmetry","R_color_variance","G_color_variance","B_color_variance","diameter","diagnostic","healthy"]
+    data = [image_id,symmetry,Rvar,gvar,bvar,diameter,"tbd","tbd"]
+
+    with open('featuresFile.csv', 'w', encoding='UTF8',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerow(data)
 
     # folder_dir = "data/images/guide_images"
     # for images in os.listdir(folder_dir):
